82a5752d2053298dd1915806cb7ec9643a734290
Kristen <kristen1286@gmail.com>
2021-03-02 02:21:51 +0000

    Completed function.
    Co-Authored-by Seale Wong <seale.wong@gmail.com>

--- ratings.py
+++ ratings.py
@@ -1,5 +1,29 @@
 
-def creating_dictionary_restaurants(textfile):
+def creating_dict_restaurant_ratings(file_name):
     """Restaurant rating lister."""
+    
+    data_file = open(file_name)
 
-# put your code here
+    restaurant_ratings = {}
+
+    for line in data_file:
+        lines_cleaned = line.rstrip().split(":")
+        restaurant_ratings[lines_cleaned[0]] = lines_cleaned[1]
+    
+    new_restaurant = input("Enter restaurant name: ")
+    new_rating = input("Enter restaurant rating: ")
+
+    restaurant_ratings[new_restaurant] = new_rating
+
+
+        
+      
+    for restaurant, rating in sorted(restaurant_ratings.items()):
+        print(f"{restaurant} is rated at {rating}.")
+
+    
+   
+
+ 
+        
+creating_dict_restaurant_ratings('scores.txt')
